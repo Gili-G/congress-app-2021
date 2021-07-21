07f9717c92d88893a11d4db18072fbb782dbdc95
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-07-21 18:03:57 -0400

    proof of concept file upload

--- SyncServer/src/main/java/example/syncSrv/Main.java
+++ SyncServer/src/main/java/example/syncSrv/Main.java
@@ -1,16 +1,63 @@
 package example.syncSrv;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Paths;
 import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
-        Flux.interval(Duration.ofSeconds(10)).flatMap(num -> runLearning())
-                .doOnNext(useless -> System.out.println("Running AI detection"))
-                .blockLast();
+        FileInputStream account = null;
+        try {
+            account = new FileInputStream("./SyncServer/congress-app-c6d54-firebase-adminsdk-l84cs-e41c97bcf7.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseOptions options = null;
+        try {
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(account))
+                    .setStorageBucket("congress-app-c6d54.appspot.com")
+                    .build();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        FirebaseApp.initializeApp(options);
+
+        BufferedImage bImage = null;
+        try {
+            bImage = ImageIO.read(new File("./SyncServer/example.jpg"));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(bImage, "jpg", bos );
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        byte [] data = bos.toByteArray();
+
+        Bucket bucket = StorageClient.getInstance().bucket();
+        bucket.create("gunDestination.jpg", data);
+
+        bucket.get("gunDestination.jpg").downloadTo(Paths.get("./SyncServer/downloaded.jpg"));
+
+        System.out.println(FirebaseApp.getInstance().getName());
+
+//        Flux.interval(Duration.ofSeconds(10)).flatMap(num -> runLearning())
+//                .doOnNext(useless -> System.out.println("Running AI detection"))
+//                .blockLast();
     }
 
     public static Mono<?> runLearning() {
